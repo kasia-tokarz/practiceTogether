f2519426b45997fd1efbb281531da9fa6458f073
kasia-tokarz <k.kolinska@gmail.com>
2020-02-24 10:57:07 +0100

    konfiguracja

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <p>
+      Hello React
+    </p>
+  );
+}
+
+export default App;
