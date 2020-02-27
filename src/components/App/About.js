b00f9b1f72ceeeef498f9a70cdbff8b9f7e6a3fe
kasia-tokarz <k.kolinska@gmail.com>
2020-02-27 12:25:24 +0100

    about

--- src/components/App/About.js
+++ src/components/App/About.js
@@ -14,7 +14,7 @@ const About = () => {
                         <h2><i class="fas fa-hashtag"></i>whatAboutMe</h2>
 
                     </div>
-                    <p>Wszytskim nam dzisiaj brakuje czasu, mieszkamy w róznych częściach naszych miast, mamy napięte grafiki, a nasze to do listy są w ciągłej gotowości.. A co gdbyśmy umawiali się z sąsiadem z bloku na przecikwo pobiegać? Nowa relacja, nowe doświadczenie, wymiana poglądów i niesamowita oszczedność czasu! Pomyśl o tym!!</p>
+                    <p></p>
                 </div>
 
 
