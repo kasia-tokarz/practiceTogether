797ce6f8fb730bb24be26c68cac2ee351367282b
kasia-tokarz <k.kolinska@gmail.com>
2020-02-28 13:08:02 +0100

    added read me

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Practice together
 Description
 -----------
 
-With our app you can connect with people in your neighbourhood as well as all around the world. Our goal is to create local communities of people motivated to get out of couch and spend an active time good for their body and mind
+With our app you can connect with people in your neighbourhood as well as all around the world. Our goal is to create local communities of people motivated to get out of couch and spend an active time good for their body and mind.
 
 Features
 -----------
@@ -14,29 +14,41 @@ With our app you can
   * create your own practive group
   * set its difficulty level (beginner, advanced, teacher)
   * work out at your house or at a different location
-  * You can find other groups in your area and request to join it
+  * you can find other groups in your area and request to join it
   * you can meet many incredible people and share your experience with them
-  * Stay Active!
+  * stay Active!
 
 
 Technologies
 ---------------
 
-We use following libraries:
+This App used following libraries:
 
    * google-maps v.4.2.3,
    * google-maps-react v.2.0.2,
    * react-geocode v.0.2.1,
    * react-burger-menu v.2.6.13,
-   * react-router-dom v.5.1.2
+   * react-router-dom v.5.1.2,
+   * React-react
     
 
 
 How to run it
 --------------
-After checkout download modules by typing `npm install`. Afterwards 
-simply type `npm start` to run application. Be aware that demo uses `local-storage`. If you want to extend this application, consider using some BE technology
-
+* for start you need to create new environment by Create React App,
+* next you neet to clone this repo by typing `git clone`,
+* next install modules by typing `npm install`,
+* for start simply type `npm start` to run application,
+* to see app go to localhost:3000,
+* be aware that demo uses `localStorage`
+ 
 
 ![Image](6nK.gif "icon")
+
+Demo
+--------------
+![Image](practiceTogether.gif "icon")
+
+Go to YouTube to see demo movie:  
+you can also visit this link LINK DO DEMO 
 
--- src/components/App/HowItWorks.js
+++ src/components/App/HowItWorks.js
@@ -13,7 +13,7 @@ const HowItWorks = () => {
                 </div>
                 <div id="textDiv">
                     <div id="headerTitle">
-                        <h2><i class="fas fa-hashtag"></i> takeCareAboutYourTime</h2>
+                        <h2><i className="fas fa-hashtag"></i> takeCareAboutYourTime</h2>
 
                     </div>
                     <p>Wszytskim nam dzisiaj brakuje czasu, mieszkamy w róznych częściach naszych miast, mamy napięte grafiki, a nasze to do listy są w ciągłej gotowości.. A co gdbyśmy umawiali się z sąsiadem z bloku na przecikwo pobiegać? Nowa relacja, nowe doświadczenie, wymiana poglądów i niesamowita oszczedność czasu! Pomyśl o tym!!</p>
@@ -28,7 +28,7 @@ const HowItWorks = () => {
             <div id="wrapperDiv">
                 <div id="textDiv">
                     <div id="headerTitle">
-                        <h2><i class="fas fa-hashtag"></i> respectYourBody</h2>
+                        <h2><i className="fas fa-hashtag"></i> respectYourBody</h2>
 
                     </div>
                     <p>Przyglądaj się swojemu ciału i patrz na to co mu słuzy. Dostosowuj rodzaj aktywności fizycznej do tego co uspokaja Twoją głowę. Jesteśmy systemem naczyń połączonych dlatego chcemy działać całościowo, łączyć aktywność fizyczną z czyszczeniem głowy a to wszytsko przekłuć w serię #ownHabbits, które małymi krokami zmienią naszą codzienność! Pamiętaj o tym! Jesteś sumą swoich myśli i nawyków!!</p>
@@ -49,7 +49,7 @@ const HowItWorks = () => {
                 </div>
                 <div id="textDiv">
                     <div id="headerTitle">
-                        <h2><i class="fas fa-hashtag"></i> makeYourCommunity</h2>
+                        <h2><i className="fas fa-hashtag"></i> makeYourCommunity</h2>
 
                     </div>
                     <p>Wiesz, ze najwiekszym problemem dzisiejszych czasów jest samotność i brak czasu? Dlatego checemy pomóc tworzyć grupy ludzi motywujących się na własnym, lokalnym podwórku. Chcemy tworzyć autentyczne wiezi, wykorzystując internet a nie trwać w nim. Chcemy wyjść wspólnie z domu, spotkać sie z freekami o tej samej zajawce i stworzyć prawdziwe  lokalne Community!!</p>
